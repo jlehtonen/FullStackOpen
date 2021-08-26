4835fb735b5006057ec2c6efae2f684d38fbcb4f
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-26 20:23:33 +0300

    Complete exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,8 +4,16 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const personExists = () => persons.map(person => person.name).indexOf(newName) !== -1;
+
   const handleSubmit = event => {
     event.preventDefault();
+
+    if (personExists()) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     setPersons([...persons, { name: newName }]);
     setNewName("");
   };
