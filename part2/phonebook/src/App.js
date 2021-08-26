cff708048ee98eeb194e2824675040ef3ff6f88e
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-26 20:18:22 +0300

    Complete exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,31 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleSubmit = event => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+
+  const handleNameChange = ({ target }) => {
+    setNewName(target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
