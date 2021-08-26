ab95944b9a64289be95301450ef6b9a3d23f8851
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-26 19:53:40 +0300

    Complete exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,31 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <strong>total of {total} exercises</strong>;
-};
-
-const Course = ({ course }) => (
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>
-);
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
