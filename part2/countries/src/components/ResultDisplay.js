fdcb5479f91278031e3ac2e46053e257667a88b4
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-27 14:10:55 +0300

    Complete exercise 2.14

--- part2/countries/src/components/ResultDisplay.js
+++ part2/countries/src/components/ResultDisplay.js
@@ -1,17 +1,57 @@
-const CountryDisplay = ({ country }) => (
-  <div>
-    <h1>{country.name}</h1>
-    <div>capital {country.capital}</div>
-    <div>population {country.population}</div>
-    <h2>languages</h2>
-    <ul>
-      {country.languages.map(language => (
-        <li key={language}>{language}</li>
-      ))}
-    </ul>
-    <img src={country.flag} height="100" alt={`Flag of ${country.name}`} />
-  </div>
-);
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const WeatherDisplay = ({ country, weather }) => {
+  if (weather === null) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Weather in {country.capital}</h2>
+      <div>
+        <b>temperature:</b> {weather.temperature} Celsius
+      </div>
+      <img src={weather.icon} alt="" height="70" />
+      <div>
+        <b>wind:</b> {weather.windSpeed} mph direction {weather.windDirection}
+      </div>
+    </div>
+  );
+};
+
+const CountryDisplay = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${country.capital}`;
+    axios.get(url).then(({ data }) => {
+      setWeather({
+        temperature: data.current.temperature,
+        windSpeed: data.current.wind_speed,
+        windDirection: data.current.wind_dir,
+        icon: data.current.weather_icons[0],
+      });
+    });
+  }, [country]);
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h2>languages</h2>
+      <ul>
+        {country.languages.map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flag} height="100" alt={`Flag of ${country.name}`} />
+      <WeatherDisplay country={country} weather={weather} />
+    </div>
+  );
+};
 
 const CountryListDisplay = ({ countries, handleClick }) => (
   <div>
