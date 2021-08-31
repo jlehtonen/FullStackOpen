c80612231d5ac448bda8a44b89c51903960845db
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-31 18:26:52 +0300

    Complete exercise 4.5

--- part4/bloglist_back/utils/list_helper.js
+++ part4/bloglist_back/utils/list_helper.js
@@ -2,7 +2,17 @@ const dummy = blogs => 1;
 
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = blogs => {
+  const blog = blogs.reduce((favorite, b) => (b.likes > favorite.likes ? b : favorite));
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
