90e074e00f8cbb4241a552c78e860d0fc424c559
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-31 19:37:51 +0300

    Complete exercise 4.8

---
+++ part4/bloglist_back/tests/blogs.test.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+test("GET /api/blogs returns correct number of blogs in JSON format", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect("Content-Type", /application\/json/);
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
