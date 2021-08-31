0e95a33e936ca2d072ab436a848eda1b9850dafe
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-08-31 18:02:28 +0300

    Complete exercise 4.2

--- part4/bloglist_back/index.js
+++ part4/bloglist_back/index.js
@@ -1,23 +1,10 @@
-const config = require("./utils/config");
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const config = require("./utils/config");
 const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogs");
 
-mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogsRouter);
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
