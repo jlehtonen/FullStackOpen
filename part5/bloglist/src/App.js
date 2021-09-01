7eb6edb9db0f536557cdfb2b4af0f2d3d1f37e3a
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-01 18:59:57 +0300

    Complete exercise 5.1

--- part5/bloglist/src/App.js
+++ part5/bloglist/src/App.js
@@ -1,22 +1,41 @@
 import { useState, useEffect } from "react";
-import Blog from "./components/Blog";
+import LoginForm from "./components/LoginForm";
+import BlogList from "./components/BlogList";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     blogService.getAll().then(blogs => setBlogs(blogs));
   }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
-    </div>
-  );
+  const handleLogin = async event => {
+    event.preventDefault();
+
+    const user = await loginService.login(username, password);
+    setUser(user);
+    setUsername("");
+    setPassword("");
+  };
+
+  if (user === null) {
+    return (
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      />
+    );
+  }
+
+  return <BlogList blogs={blogs} user={user} />;
 };
 
 export default App;
