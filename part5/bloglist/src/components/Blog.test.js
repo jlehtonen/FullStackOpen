5453f05cbde1ea16f2ebc6b2dae0d36cadd989e2
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-01 22:54:50 +0300

    Complete exercise 5.14

--- part5/bloglist/src/components/Blog.test.js
+++ part5/bloglist/src/components/Blog.test.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Blog from "./Blog";
 
 test("by default, Blog renders title and author but not url or likes", () => {
@@ -27,4 +27,34 @@ test("by default, Blog renders title and author but not url or likes", () => {
   expect(component.container).toHaveTextContent("author");
   expect(component.container).not.toHaveTextContent("url");
   expect(component.container).not.toHaveTextContent("0");
+});
+
+test("Blog renders title, author, url and likes when the button to show all information has been pressed", () => {
+  const blog = {
+    title: "title",
+    author: "author",
+    url: "url",
+    likes: 0,
+    user: {
+      id: "123",
+      name: "name",
+      username: "username",
+    },
+  };
+  const component = render(
+    <Blog
+      blog={blog}
+      loggedUser={{ username: "username" }}
+      handleLikeClick={jest.fn()}
+      handleDelete={jest.fn()}
+    />
+  );
+
+  const button = component.getByText("view");
+  fireEvent.click(button);
+
+  expect(component.container).toHaveTextContent("title");
+  expect(component.container).toHaveTextContent("author");
+  expect(component.container).toHaveTextContent("url");
+  expect(component.container).toHaveTextContent("0");
 });
