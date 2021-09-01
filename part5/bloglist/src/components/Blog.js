bafa85c0ef4504f00c7c19aff5609581157e27a0
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-01 20:27:42 +0300

    Complete exercises 5.6 - 5.7

--- part5/bloglist/src/components/Blog.js
+++ part5/bloglist/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const styles = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (!isExpanded) {
+    return (
+      <div style={styles}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setIsExpanded(true)}>view</button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={styles}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setIsExpanded(false)}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>
+  );
+};
 
 export default Blog;
