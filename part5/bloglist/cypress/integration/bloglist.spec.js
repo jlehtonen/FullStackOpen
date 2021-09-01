894837e07312264d6a7db2a44f8f7b73e82a6959
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-01 23:37:48 +0300

    Complete exercise 5.18

--- part5/bloglist/cypress/integration/bloglist.spec.js
+++ part5/bloglist/cypress/integration/bloglist.spec.js
@@ -1,10 +1,31 @@
 describe("Bloglist app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
+    cy.request("POST", "http://localhost:3001/api/users", {
+      username: "username",
+      password: "password",
+      name: "Name",
+    });
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
     cy.get("#login-form").should("exist");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("username");
+      cy.get("#password").type("password");
+      cy.get("#login-button").click();
+      cy.get("#login-form").should("not.exist");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("username");
+      cy.get("#password").type("wrong password");
+      cy.get("#login-button").click();
+      cy.get("#login-form").should("exist");
+    });
   });
 });
