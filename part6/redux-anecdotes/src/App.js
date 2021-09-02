d15ae97b4bf168cb2e15b37502a65e482b03d3f2
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-02 19:18:44 +0300

    Complete exercise 6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { vote, create } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector(state => state).sort((a, b) => b.votes - a.votes);
   const dispatch = useDispatch();
 
   const addAnecdote = event => {
