cf6db03817cd6de9fe3a94dacabdf3fc7c45bfdc
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-06 15:59:07 +0300

    Complete exercise 8.5

--- part8/library_back/index.js
+++ part8/library_back/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -111,8 +111,18 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) =>
-      args.author ? books.filter(book => book.author === args.author) : books,
+    allBooks: (root, args) => {
+      let filteredBooks = books;
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(b => b.author === args.author);
+      }
+
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter(b => b.genres.includes(args.genre));
+      }
+
+      return filteredBooks;
+    },
     allAuthors: () => authors,
   },
 
