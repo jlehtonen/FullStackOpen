8dcb9d93bbe0eb75915b9841f242a498c69327f8
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-06 21:23:04 +0300

    Complete exercise 8.7

--- part8/library_back/index.js
+++ part8/library_back/index.js
@@ -109,6 +109,7 @@ const typeDefs = gql`
 
   type Mutation {
     addBook(title: String!, author: String!, published: Int!, genres: [String!]!): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -144,6 +145,16 @@ const resolvers = {
 
       books = [...books, book];
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map(a => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
