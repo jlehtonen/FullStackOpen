ea3164cc03717496ec49e29d617f61d5fe47b819
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-09 18:22:37 +0300

    Complete exercise 8.19

--- part8/library_front/src/components/Books.js
+++ part8/library_front/src/components/Books.js
@@ -1,19 +1,48 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 
+const getAllGenres = books => {
+  const genres = [];
+  books.forEach(book => {
+    book.genres.forEach(genre => {
+      if (!genres.includes(genre)) {
+        genres.push(genre);
+      }
+    });
+  });
+  return genres;
+};
+
 const Books = props => {
   const result = useQuery(ALL_BOOKS);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   if (!props.show || result.loading) {
     return null;
   }
 
   const books = result.data.allBooks;
+  const filteredBooks = books.filter(book => {
+    if (!selectedGenre) {
+      return true;
+    }
+
+    return book.genres.includes(selectedGenre);
+  });
+
+  const genres = getAllGenres(books);
+
+  const selectedGenreStyle = { outline: "2px solid blue" };
 
   return (
     <div>
       <h2>books</h2>
-
+      {selectedGenre ? (
+        <p>
+          in genre <b>{selectedGenre}</b>
+        </p>
+      ) : null}
       <table>
         <tbody>
           <tr>
@@ -21,7 +50,7 @@ const Books = props => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map(a => (
+          {filteredBooks.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -30,6 +59,23 @@ const Books = props => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.map(genre => (
+          <button
+            key={genre}
+            style={selectedGenre === genre ? selectedGenreStyle : null}
+            onClick={() => setSelectedGenre(genre)}
+          >
+            {genre}
+          </button>
+        ))}
+        <button
+          style={!selectedGenre ? selectedGenreStyle : null}
+          onClick={() => setSelectedGenre(null)}
+        >
+          all genres
+        </button>
+      </div>
     </div>
   );
 };
