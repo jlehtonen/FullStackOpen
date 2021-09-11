b768225878828faf545b0aa8b476245857e11f6a
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-11 18:49:50 +0300

    Complete exercise 9.5

--- part9/ts-exercises/index.ts
+++ part9/ts-exercises/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
+import { isNumber } from "./util";
+
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  if (!isNumber(req.query.height) || !isNumber(req.query.weight)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  return res.json({ weight, height, bmi: calculateBmi(height, weight) });
 });
 
 const PORT = 3003;
