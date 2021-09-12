81ec53c52b2a6a2b9fa861dae8d9744a311bf447
Jaakko Lehtonen <jaakko.lehtonen@yahoo.com>
2021-09-12 16:44:05 +0300

    Complete exercise 9.15

--- part9/ts-courseinfo/src/App.tsx
+++ part9/ts-courseinfo/src/App.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 
-interface HeaderProps {
-  name: string;
-}
-
-interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartWithDescription {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartWithDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseWithRequirements extends CoursePartWithDescription {
+  type: "special";
+  requirements: Array<string>;
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseWithRequirements;
+
+interface HeaderProps {
+  name: string;
 }
 
 interface ContentProps {
@@ -17,6 +46,61 @@ interface TotalProps {
   courseParts: Array<CoursePart>;
 }
 
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+};
+
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.type) {
+    case "normal":
+      return (
+        <div>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>
+            <i>{part.description}</i>
+          </div>
+        </div>
+      );
+    case "groupProject":
+      return (
+        <div>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>project exercises {part.groupProjectCount}</div>
+        </div>
+      );
+    case "submission":
+      return (
+        <div>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>
+            <i>{part.description}</i>
+          </div>
+          <div>submit to {part.exerciseSubmissionLink}</div>
+        </div>
+      );
+    case "special":
+      return (
+        <div>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+          <div>
+            <i>{part.description}</i>
+          </div>
+          <div>required skills: {part.requirements.join(", ")}</div>
+        </div>
+      );
+    default:
+      return assertNever(part);
+  }
+};
+
 const Header = ({ name }: HeaderProps) => {
   return <h1>{name}</h1>;
 };
@@ -26,7 +110,7 @@ const Content = ({ courseParts }: ContentProps) => {
     <>
       {courseParts.map(part => (
         <p key={part.name}>
-          {part.name} {part.exerciseCount}
+          <Part part={part} />
         </p>
       ))}
     </>
@@ -47,18 +131,38 @@ const Total = ({ courseParts }: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special",
     },
   ];
 
